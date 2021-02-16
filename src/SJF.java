89910c3f2fcefa1971669b68c5d3e4f2aad4856d
jdibble20 <jdibble@bsu.edu>
2021-02-16 15:03:14 -0500

    Rewrite SJF

--- src/SJF.java
+++ src/SJF.java
@@ -21,53 +21,28 @@ public class SJF implements Algorithm
     }
 
     public void schedule(){
-        System.out.println("Shortest-Job First scheduling");
-        int totalWaitingTime = 0;
-
-        Task currentTask;
-        while (!queue.isEmpty()) {
-
-            currentTask = pickNextTask();
-            int wTime = 0;
-            if (CPU.getCurrentTime() > currentTask.getArrivalTime()){
-                wTime = CPU.getCurrentTime() - currentTask.getArrivalTime();
-            }
-            totalWaitingTime += wTime;
-
-            CPU.run(currentTask, currentTask.getBurst());
-
-            System.out.println(currentTask.getName() + " finished at time "+CPU.getCurrentTime() + ". Its waiting time is: " + wTime);
-            queue.remove(currentTask);
-        }
-        //need to cast either the numerator or the denominator to the double type
-        //otherwise, when both are integers, their division result will always be rounded to integer
-        double averageWaitingTime = totalWaitingTime / (double) numTasks;
-        //use printf for formatted out (only show two digits after decimal points)
-        System.out.printf("\nThe average waiting time is: %.2f\n", averageWaitingTime);
+        chooseShortestTask();
     }
 
     public Task pickNextTask() {
         return queue.get(taskIndex);
     }
 
-    public Task chooseShortestTask(ArrayList<Task> tasks){
-        Task shortestTask = tasks.get(0);
-        ArrayList<Integer> burstList = new ArrayList<>();
-        for(int i=1; i<queue.size(); i++){
-            if(tasks.get(i).getBurst() < shortestTask.getBurst()){
-                shortestTask = tasks.get(i);
+    public void chooseShortestTask(){
+        // get current queue burst lengths and indices
+        HashMap<Integer,Integer> burstVals = new HashMap<>();
+        for(int i=0; i < queue.size(); i++){
+            burstVals.put(i, queue.get(i).getBurst());
+        }
+        int minValue = 0;
+        for(int i=0; i < burstVals.size() - 1; i++){
+            minValue = burstVals.get(i);
+            if(burstVals.get(i+1) < minValue){
+                minValue = burstVals.get(i+1);
             }
         }
-        return shortestTask;
+        System.out.println(minValue);
     }
 
-    public ArrayList<Task> getAvailableJobs(){
-        ArrayList<Task> validTasks = new ArrayList<>();
-        for (Task task : queue) {
-            if (CPU.getCurrentTime() <= task.getArrivalTime()) {
-                validTasks.add(task);
-            }
-        }
-        return validTasks;
-    }
+
 }
